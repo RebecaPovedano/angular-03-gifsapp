d8567d43509637689cda1d856b6d8c45e824cf76
RebecaPovedano <git@stackblitz.com>
2024-10-28 11:30:58 +0100

    Initial commit

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,50 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GifsService {
+  private _tagsHistory: string[] = [];
+  private apikey: string = 'uH36n7Rv0qsdYHn9zvTfa2KV9iYp7e1C';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+  public gifList: Gif[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  get tagsHistory() {
+    return [...this._tagsHistory];
+  }
+
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
+    }
+
+    this._tagsHistory.unshift(tag);
+
+    this._tagsHistory = this.tagsHistory.splice(0, 10);
+  }
+
+  public searchTag(tag: string): void {
+    if (tag.length === 0) return;
+
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apikey)
+      .set('q', tag)
+      .set('limit', 10);
+
+    this.http
+      .get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe((resp) => {
+        this.gifList = resp.data;
+        console.log(this.gifList);
+      });
+  }
+}
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+export default {
+  server: {
+    watch: {
+      usePolling: false,
+    },
+  },
+};
